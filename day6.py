931a4e99be921b362194fa655006301224002f7a
DhirendraDhangar <154353863+DhirendraDhangar@users.noreply.github.com>
2025-06-20 21:33:45 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,15 @@
+x = 10 
+print(type(x))
+y = 8.8 
+print(type(y))
+z = 5 + 2j
+print(type(z))
+
+# string
+name = ' dhirendra'
+myself = " student"
+college = '''lakshmi narain   college of technology 
+                  excellence'''
+print(name)
+print(myself)
+print(college)
